18aed30459997c69188db3caab862c48a6361285
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-29 15:50:28 -0600

    Create 04_Lists_Predict_2.py

---
+++ 04_Lists_Predict_2.py
@@ -0,0 +1,19 @@
+# filename: 04_Lists_Predict_2.py
+"""
+Activity: Predict the Output
+
+1.  Read the code below carefully.
+2.  Without running it, what do you think the `print` statements will output?
+    Write your predictions in the `prediction_1`, `prediction_2`, and `prediction_3` variables.
+3.  Remember that list indexing starts at 0!
+"""
+
+prediction_1 = "" # What will colors[1] be?
+prediction_2 = "" # What will colors[0] be?
+prediction_3 = "" # What will colors[-1] be?
+
+colors = ["red", "green", "blue", "yellow"]
+
+print("The color at index 1 is:", colors[1])
+print("The first color is:", colors[0])
+print("The last color is:", colors[-1])

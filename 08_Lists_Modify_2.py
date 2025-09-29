fba5f0497625d4c142df938482c94c0718096070
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-29 15:51:37 -0600

    Make movie list interactive with user input
    Modified the program to start with an empty list and allow user input for favorite movies.

---
+++ 08_Lists_Modify_2.py
@@ -0,0 +1,25 @@
+# filename: 08_Lists_Modify_2.py
+"""
+Activity: Modify the Code
+
+This program has a hardcoded list of movies. Your goal is to make it interactive!
+
+Your goal:
+1.  Modify the code so that instead of a predefined list, it starts with an *empty* list.
+2.  Use the `input()` function to ask the user for their favorite movie.
+3.  Use the `append()` method to add the user's input to your empty list.
+4.  Run the program and enter a movie to see if it works!
+"""
+
+# --- MODIFY THE CODE BELOW ---
+
+# 1. Create an empty list called `movies`
+movies = ["The Matrix", "Finding Nemo", "The Lord of the Rings"]
+
+# 2. Get input from the user
+
+# 3. Add the user's input to the list
+
+# --- END MODIFICATION ---
+
+print("Here is your list of movies:", movies)
